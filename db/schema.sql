a5aa7ed421f838db6371d82e825dadcded1f5056
Kalamath <thecodedimage@gmail.com>
2019-09-20 18:53:39 -0700

    working on handlebars

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Drops the db if it exists currently --
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Extra Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Bacon Ranch Burger', false);

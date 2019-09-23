fb2362a257c887caaa43d4b86b31be4e48cc2792
Kalamath <thecodedimage@gmail.com>
2019-09-22 22:15:56 -0700

    trying to fix Method Not Allowed bug

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
     var burgerObject = {
       burgers: data
     };
-    console.log(burgerObject);
+    // console.log(burgerObject);
     res.render("index", burgerObject);
   });
 });
